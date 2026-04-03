e4f18381f149262c1eec6bf97adcc550cc777ac5
mhenrixon <mikael@mhenrixon.com>
2026-04-03 07:08:13 +0200

    test(web): add system tests for queue delete, show page pause/resume
    - Index: purge, delete, pause, resume buttons render correctly
    - Index: paused queues show resume button and paused badge
    - Show: purge, delete, pause buttons render
    - Show: paused state shows resume button and paused badge
    - Show: displays queue metrics, messages, and empty state
    - StubDataSource: make paused_queues configurable for tests

--- spec/system/support/data_source_stub.rb
+++ spec/system/support/data_source_stub.rb
@@ -4,7 +4,8 @@ module Pgbus
   module Test
     class StubDataSource
       attr_accessor :stats, :queues, :processes_list, :failed_events_list,
-                    :dlq_messages_list, :events_list, :subscribers_list, :jobs_list
+                    :dlq_messages_list, :events_list, :subscribers_list, :jobs_list,
+                    :paused_queues
 
       def initialize
         @stats = default_stats
@@ -15,6 +16,7 @@ module Pgbus
         @events_list = []
         @subscribers_list = []
         @jobs_list = []
+        @paused_queues = []
       end
 
       def summary_stats = @stats
@@ -33,7 +35,7 @@ module Pgbus
       def jobs(queue_name: nil, page: 1, per_page: 25) = @jobs_list
       def purge_queue(_name) = true
       def drop_queue(_name) = true
-      def queue_paused?(_name) = false
+      def queue_paused?(name) = @paused_queues.include?(name)
       def pause_queue(_name, reason: nil) = true
       def resume_queue(_name) = true
       def retry_failed_event(_id) = true
